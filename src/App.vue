57333832be0e44fd5b29020887e54c280b57a7b5
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-29 12:21:19 -0500

    init structure app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <PokemonGame />
   </div>
 </template>
+<script lang="ts" setup>
+import PokemonGame from '@/modules/pokemon/pages/PokemonGame.vue';
+</script>
---
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -0,0 +1,17 @@
+<template>
+   <section class="flex flex-col justify-center items-center h-screen">
+      <h1 class="text-3xl">awaiting please</h1>
+      <h3 class="animate-pulse">Loading pokemons...</h3>
+   </section>
+   <section class="flex flex-col justify-center items-center h-screen">
+      <h1 class="m-4">Who is the pokemon?</h1>
+      <!-- Pokemon pictures -->
+      <PokemonPicture/>
+      <!-- Pokemons options -->
+       <PokemonOptions/>
+   </section>
+</template>
+<script lang="ts" setup>
+import PokemonPicture from '@/modules/pokemon/components/PokemonPicture.vue'
+import PokemonOptions from '@/modules/pokemon/components/PokemonOptions.vue'
+</script>
