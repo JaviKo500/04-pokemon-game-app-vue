108307dc855fe9939240cadf7d38816706cd9f85
JaViKo500 <javikogutierrez64@gmail.com>
2024-12-29 13:06:40 -0500

    get options and properties computed and random sort

--- src/modules/pokemon/composables/usePokemonGame.ts
+++ src/modules/pokemon/composables/usePokemonGame.ts
@@ -1,21 +1,49 @@
-import { onMounted, ref } from "vue";
-import { GameStatus, type PokemonResponse } from '../interfaces';
+import { computed, onMounted, ref } from "vue";
+import { GameStatus, type Pokemon, type PokemonResponse } from '../interfaces';
 import { pokemonApi } from "../api/pokemonApi";
+import { sleep } from "../helpers";
 
 export const usePokemonGame = () => {
 
    const gameStatus = ref<GameStatus>(GameStatus.Playing);
+   const pokemons = ref<Pokemon[]>([]);
+   const pokemonsOptions = ref<Pokemon[]>([]);
+   const isLoading = computed(() => pokemons.value.length === 0);
 
-   const getPokemons = async () => {
+
+   const getPokemons = async (): Promise<Pokemon[]> => {
       const response = await pokemonApi.get<PokemonResponse>('/?limit=151');
-      console.log(response.data);
+      const pokemonsArray = response.data.results?.map((pokemon) => {
+         const urlParts: string[] = pokemon?.url?.split('/') ?? [];
+         const id = urlParts[urlParts.length - 2] ?? Date.now();
+         return {
+            id: +id,
+            name: pokemon.name!,
+         } as Pokemon;
+      });
+
+      return (pokemonsArray ?? []).sort(() => Math.random() - 0.5);
    };
 
-   onMounted(() => {
-      getPokemons();
+   const getNextOptions = async (howMany: number = 4) => {
+      gameStatus.value = GameStatus.Playing;
+      pokemonsOptions.value = pokemons.value.slice(0, howMany);
+      pokemons.value = pokemons.value.slice(howMany);
+   }
+   onMounted(async () => {
+      await sleep(1);
+      pokemons.value = await getPokemons();
+      getNextOptions();
+      console.log('<--------------- JK UsePokemonGame --------------->');
+      console.log(pokemonsOptions.value);
    });
 
    return {
       gameStatus,
+      isLoading,
+      pokemonsOptions,
+
+      // methods
+      getNextOptions,
    };
 }
--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,9 +1,9 @@
 <template>
-   <section class="flex flex-col justify-center items-center h-screen">
+   <section v-if="isLoading"  class="flex flex-col justify-center items-center h-screen">
       <h1 class="text-3xl">awaiting please</h1>
       <h3 class="animate-pulse">Loading pokemons...</h3>
    </section>
-   <section class="flex flex-col justify-center items-center h-screen">
+   <section v-else class="flex flex-col justify-center items-center h-screen">
       <h1 class="m-4">Who is the pokemon?</h1>
       <!-- Pokemon pictures -->
       <PokemonPicture/>
@@ -16,5 +16,5 @@
    import PokemonOptions from '@/modules/pokemon/components/PokemonOptions.vue'
    import { usePokemonGame } from '@/modules/pokemon/composables/usePokemonGame';
 
-   const { gameStatus } = usePokemonGame();
+   const { isLoading } = usePokemonGame();
 </script>
